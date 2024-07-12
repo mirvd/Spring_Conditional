1cf41f2c6be8d3ccf5cab43f3a70177c9a4c60dd
mirvd <mir3227@mail.ru>
2024-07-12 18:58:09 +0500

    first commit

---
+++ src/main/java/ru/netollogy/springboot/SystemProfile.java
@@ -0,0 +1,4 @@
+package ru.netollogy.springboot;
+public interface SystemProfile {
+    String getProfile();
+}
---
+++ src/main/java/ru/netollogy/springboot/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netollogy.springboot.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netollogy.springboot.DevProfile;
+import ru.netollogy.springboot.ProductionProfile;
+import ru.netollogy.springboot.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+
+}
